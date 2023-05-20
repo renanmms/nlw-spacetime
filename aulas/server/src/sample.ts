6e9db64a020d977fef5cfcb2800afb906a06287c
Renan Martins <renanmartins1999@hotmail.com>
2023-05-19 22:46:56 -0300

    Inicializando repositorio

---
+++ aulas/server/src/sample.ts
@@ -0,0 +1,15 @@
+interface Person {
+    name: string,
+    age: number,
+    weight: number,
+    height: number
+}
+
+const person = {
+    name: "Renan",
+    age: 23,
+    weight: 60.0,
+    height: 170.0
+}
+
+console.log(person);
---
+++ aulas/server/src/server.js
@@ -0,0 +1,14 @@
+// interface Pointlike {
+//     x: number;
+//     y: number;
+//   }
+//   interface Named {
+//     name: string;
+//   }
+var person = {
+    name: "Renan",
+    age: 20,
+    weight: 55.0,
+    height: 165.0
+};
+console.log(person);
